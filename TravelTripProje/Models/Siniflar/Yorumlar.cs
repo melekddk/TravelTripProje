6a80f048bd71cb9d8fd7b0ee46cffddd5b4d92a0
melek Dedik <melekdedik@gmail.com>
2021-02-11 22:31:01 +0300

    Add project files.

---
+++ TravelTripProje/Models/Siniflar/Yorumlar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Yorumlar
+    {
+        [Key]
+        public int ID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Mail { get; set; }
+        public string Yorum { get; set; }
+        public Blog Blog { get; set; } //2.kısımda yazılan blog 1. yazılandan yani blog tablosundan gelen değeri tutuyor.
+
+    }
+}
